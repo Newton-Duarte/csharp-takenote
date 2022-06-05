20439ff9b12cfccf0911367845e2bef36d2350d1
Newton Duarte <newton_@hotmail.com>
2022-06-05 12:08:35 -0300

    refactor: program class to use menu class methods, add list of notes variable and method, add create note method

--- Program.cs
+++ Program.cs
@@ -1,57 +1,27 @@
-﻿static void Menu()
+﻿namespace TakeNote
 {
-    Console.Clear();
-
-    Console.WriteLine("Bem-vindo ao TakeNote!");
-    Console.WriteLine("======================");
-    Console.WriteLine("Escolha uma opção: ");
-    Console.WriteLine("1 - Adicionar Anotação");
-    Console.WriteLine("2 - Listar Anotações");
-    Console.WriteLine("3 - Excluir Anotação");
-    Console.WriteLine("4 - Exportar em Arquivo");
-    Console.WriteLine("0 - Sair");
-
-    short option = short.Parse(Console.ReadLine());
-
-    HandleChoseOption(option);
-}
-
-static void HandleChoseOption(short option)
-{
-    switch(option)
+    class Program
     {
-        case 0: Exit(); break;
-        case 1: AddNote(); break;
-        case 2: ListNotes(); break;
-        case 3: DeleteNote(); break;
-        case 4: ExportNotes(); break;
-        default: Menu(); break;
+        static List<Note> Notes = new List<Note>();
+        static void Main(string[] args)
+        {
+            Menu.Show();
+        }
+
+        public static void ListNotes()
+        {
+            foreach(var note in Notes)
+            {
+                Console.WriteLine($"{note.Id} | {note.CreatedAt.ToString("dd/MM/yyyy hh:mm")} | {note.Title} | {note.Description}");
+            }
+
+            Menu.Show();
+        }
+
+        public static void CreateNote(Note note)
+        {
+            Notes.Add(note);
+            Menu.Show();
+        }
     }
 }
-
-static void Exit()
-{
-    Environment.Exit(0);
-}
-
-static void AddNote()
-{
-    Console.WriteLine("Você escolheu adicionar uma anotação!");
-}
-
-static void ListNotes()
-{
-    Console.WriteLine("Você escolheu listar todas as anotações!");
-}
-
-static void DeleteNote()
-{
-    Console.WriteLine("Você escolheu deletar uma anotação!");
-}
-
-static void ExportNotes()
-{
-    Console.WriteLine("Você escolheu exportar as anotações em arquivo");
-}
-
-Menu();
